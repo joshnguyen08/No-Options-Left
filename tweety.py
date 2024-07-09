d31c1bb31bfba8e0e19fbeb8f984db2997c7e281
Josh N <joshnguyen008@gmail.com>
2024-07-09 14:41:02 -0400

    Added randomization for grabbing pictures from GALLERY_DIR

--- tweety.py
+++ tweety.py
@@ -31,12 +31,26 @@ GALLERY_DIR = os.getenv('TWITTER_GALLERY_DIR')
 base_texts = [
     "GUARENTEED LOCKS... WE DON'T MISS.\n\nFREE, JOIN TODAY..\n\n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\n\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
     "CASH IT!!! 🔥🔥🔥\n\nANOTHER FIRE CALLOUT FOR THE DISCORD\n\nhttps://discord.gg/tWTZufybQ8\n\n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷 #soccer #easymoney #parley #betting #win #wingbig #sportspick #fantasysports #DFS #fantasy #cashprizes",
-    "BANG!!! CASH THIS... FREE CALLOUTS IN THE DISCORD. JOIN TODAY!\n\nPREMIUM IS FREE, ALL YOU HAVE TO DO IS FOLLOW!\nJOIN HERE ⬇️\nhttps://discord.gg/fDmVwEZYu4\n\n#sportsbettor , #optionstrading , #gme , #amc , #prizepicks #nba #cash #money",
-    "Nothing but consistency green slips in the discord when it comes to #Soccer JOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
-    "OPTIONS, SPORTS BETTING, INVESMENTS, WE GOT IT ALL.. \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
-    "WE CAN'T MISS AT ALL IN ANY REALMS \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
-    "JOIN OUR WHOLESOME COMMUNITY TODAY! \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
-    "PROFESSIONAL AND FREE COMMUNITY FOR WINNERS \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    "BANG!!! CASH THIS... FREE CALLOUTS IN THE DISCORD. \n\nJOIN TODAY!\n\nPREMIUM IS FREE, ALL YOU HAVE TO DO IS FOLLOW!\nJOIN HERE ⬇️\nhttps://discord.gg/fDmVwEZYu4\n\n#sportsbettor , #optionstrading , #gme , #amc , #prizepicks #nba #cash #money",
+    "Nothing but consistency green slips in the discord\n\n JOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    "OPTIONS, SPORTS BETTING, INVESMENTS, WE GOT IT ALL..\n \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    "WE CAN'T MISS AT ALL IN ANY REALMS\n \n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    "JOIN OUR WHOLESOME COMMUNITY TODAY! \n\n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    "PROFESSIONAL AND FREE COMMUNITY FOR WINNERS \n\n#PrizePickscfb #sportsbetting #stocks #options #prizepicks #nba📷📷 #soccer #easymoney #parley #betting #win #sportspick #DFS #fantasy #cashprizes\nJOIN HERE ⬇️\nhttps://discord.gg/TdEyQqwxp2",
+    """WE OFFER:
+
+Top stocks pick for long-term growth📈📈
+
+Best options trading plays💰💰💰
+
+Prizepicks locks 🔒🔒🔒
+
+Chance to hit big on meme coins! 🪙🪙🪙
+
+⬇️⬇️⬇️⬇️JOIN NOW ⬇️⬇️⬇️⬇️
+
+Join here on dis(cord): TdEyQqwxp2""",
+
 
 ]
 
@@ -52,17 +66,29 @@ def tweet_image(image_path):
 
 def main():
     images = os.listdir(GALLERY_DIR)
+    files = []
+    for file in images:
+        full_path = os.path.join(GALLERY_DIR, file)
+        if os.path.isfile(full_path):
+            files.append(file)
     
-    for image_file in images:
-        image_path = os.path.join(GALLERY_DIR, image_file)
-        if os.path.isfile(image_path):
-            tweet_image(image_path)
-            print(f'Tweeted: {image_path}')
-            delay = random.randint(60, 120)
+    
+    if not files:
+        print("No files found in the directory.")
+        return
+
+    while True:
+        random_file = random.choice(files)
+        random_file_path = os.path.join(GALLERY_DIR, random_file)
+        
+        if os.path.isfile(random_file_path):
+            tweet_image(random_file_path)
+            print(f'Tweeted: {random_file_path}')
+            delay = random.randint(180, 360)
             print(f'Waiting for {delay} seconds before the next tweet...')
             time.sleep(delay)
         else:
-            print(f'Image not found: {image_path}')
-
+            print(f'Image not found: {random_file_path}')
+            
 if __name__ == "__main__":
     main()
